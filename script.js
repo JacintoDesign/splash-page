80f72311b5883119e31ff1be0d0f1ad414f1423d
JACINTO WONG <jacinto.wong@cbc.ca>
2020-06-24 16:21:32 -0400

    final production code

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const { body } = document;
 
 function changeBackground(number) {
+  // Check if background already showing
   let previousBackground;
   if (body.className) {
     previousBackground = body.className;
   }
+  // Reset background
   body.className = '';
+  // If background already on, turn off, else turn on background
   switch (number) {
     case '1':
       return (previousBackground === 'background-1' ? false : body.classList.add('background-1'));
